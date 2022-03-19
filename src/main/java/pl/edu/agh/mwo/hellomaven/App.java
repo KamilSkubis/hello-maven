02fd7fd7dc6323c08fa56b33fa8631fb12e9352f
kskubis <kskubis@d17-428-pc-08.ipa.ki.agh.edu.pl>
2022-03-19 14:11:24 +0100

    done

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,6 +1,12 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Simple main application.
@@ -8,7 +14,18 @@ import java.util.Random;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        final int width = 120;
+        final int height = 20;
+
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(width).height(height);
+        builder.element(new PseudoText("Hello World"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
+
     }
 
     /**
@@ -19,6 +36,6 @@ public class App {
         // chosen by fair dice roll.
         // guaranteed to be random
         Random random = new Random();
-        return random.nextInt();;
+        return random.nextInt();
     }
 }
