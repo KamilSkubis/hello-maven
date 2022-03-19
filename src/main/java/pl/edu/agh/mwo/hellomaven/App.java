f0e9d504ef3a71f202a272a06c904a2bb8e977c2
kskubis <kskubis@d17-428-pc-08.ipa.ki.agh.edu.pl>
2022-03-19 13:59:27 +0100

    save

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,17 +1,24 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import java.util.Random;
+
 /**
- * Simple main application
+ * Simple main application.
  */
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
+
+    /**
+     * Method will return random number.
+     * @return random integer
+     */
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        Random random = new Random();
+        return random.nextInt();;
     }
 }
